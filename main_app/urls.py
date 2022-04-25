9cb38b6fd46276a1ef75efaf6c25f0e17f9ce2da
Peter Luong <peterluong@Peters-MBP.socal.rr.com>
2022-04-25 14:22:27 -0700

    urls - added route for showing camera details

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('cameras/', views.cameras_index, name='index'),
+    path('cameras/<int:camera_id>', views.cameras_detail, name='detail')
 ]
