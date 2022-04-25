56ac37c89754c6c07226a230e640db64ecb90a8f
Peter Luong <peterluong@Peters-MBP.socal.rr.com>
2022-04-25 12:59:21 -0700

    finished part 1 of lab

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cameras_index(request):
+    return render(request, 'cameras/index.html', { 'cameras': cameras })
