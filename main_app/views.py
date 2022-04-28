deaa24bce868d42cc942e6f3c5fc6572ac5fa3b0
Peter Luong <peterluong@Peters-MBP.socal.rr.com>
2022-04-28 09:37:11 -0700

    views.py -> adjusted imports, added function add_photo

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Camera, Lens, Recall
+import uuid
+import boto3
+from .models import Camera, Lens, Recall, Photo
 from .forms import RecallForm
 
-# Add the following import
-from django.http import HttpResponse
-
+S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+BUCKET = 'cameracollector'
 
 # Define the home view
 def home(request):
@@ -77,3 +78,21 @@ def assoc_lens(request, camera_id, lens_id):
   # Note that you can pass a toy's id instead of the whole object
   Camera.objects.get(id=camera_id).lens.add(lens_id)
   return redirect('detail', camera_id=camera_id)
+
+def add_photo(request, camera_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            Photo.objects.create(url=url, camera_id=camera_id)
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', camera_id=camera_id)
